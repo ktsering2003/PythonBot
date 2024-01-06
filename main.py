41a636f45703bde1812988314ca79f3a50a4e648
Kunsang Tsering <81478034+ktsering2003@users.noreply.github.com>
2024-01-06 00:27:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+# main.py
+
 from bot import related, send_message
+from api import get_weather
 
 if __name__ == "__main__":
+    # Main script for interacting with the bot
     print("BOT: Hello! What is your name!")
     user_name = input()
 
     print(f"BOT: Nice meeting you {user_name}!")
-    print("BOT: Ask a question!\nExamples:\n- Whats the weather\n- Play Game\n- Are you a robot?")
-    # \n means to use a new line
+    print("BOT: Ask a question!\nExamples:\n- What's the weather\n- Play Game\n- Are you a robot?")
+
     while True:
-        my_input = input() #input will ask you for a response in terminal
-        my_input = my_input.lower() #puts your question into lower case, so that any weird capitalization situations don't cause an unwanted response
+        my_input = input().lower()
+        related_text = related(my_input)
 
-        related_text = related(my_input) #puts your inputted message in to the related function to find which question key in responses is similar to whatever your input was
-        send_message(related_text) #bot sends a response depending on what your input was
+        if related_text == "whats today's weather?":
+            # If the user asks about the weather, get weather information and send the message
+            weather_info = get_weather()
+            send_message(weather_info)
+        else:
+            # Otherwise, send the response based on the user input
+            send_message(related_text)
 
-        if my_input == "exit": #exits out of the program if you type out exit as your message
-            exit() 
+        if my_input == "exit":
+            # If the user inputs 'exit', exit the program
+            exit()
